f6bdd9e72131a654a97e2d1de2441af19f850eef
furtadomn <mnfurtado@yahoo.com.br>
2022-04-21 19:31:07 -0300

    create greeting file

---
+++ greeting/greeting.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// func main() {
+// 	greeting()
+// }
+
+func main() {
+	var name string = "Marcella"
+	var age int = 28
+
+	fmt.Print("My name is ", name)
+	fmt.Print(". I'm ", age)
+	fmt.Print(" years old.")
+}
