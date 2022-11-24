111b5783ffe58db6dc7fd4d855da6a1e5c76a7a5
furtadomn <mnfurtado@yahoo.com.br>
2022-11-24 08:27:19 -0300

    create 'Depositar' method

--- Go: Orientação a Objetos/Banco/main.go
+++ Go: Orientação a Objetos/Banco/main.go
@@ -19,13 +19,24 @@ func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
 	}
 }
 
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito > 0 {
+		c.saldo += valorDoDeposito
+		return "Depósito realizado com sucesso! Saldo em conta:", c.saldo
+	} else {
+		return "O valor do depósito não pode ser negativo! Saldo em conta:", c.saldo
+	}
+}
+
 func main() {
 	contaDaMarcella := ContaCorrente{}
 	contaDaMarcella.titular = "Marcella"
 	contaDaMarcella.saldo = 500
 
 	fmt.Println(contaDaMarcella)
-	fmt.Println(contaDaMarcella.Sacar(800))
 	fmt.Println(contaDaMarcella.Sacar(400))
 	fmt.Println(contaDaMarcella)
+
+	status, valor := contaDaMarcella.Depositar(500)
+	fmt.Println(status, valor)
 }
