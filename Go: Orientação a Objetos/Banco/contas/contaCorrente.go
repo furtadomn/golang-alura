5eef0b4e0194c6cf6f191627595cb9de19062037
furtadomn <mnfurtado@yahoo.com.br>
2022-11-24 08:46:52 -0300

    create 'contas' package

---
+++ Go: Orientação a Objetos/Banco/contas/contaCorrente.go
@@ -0,0 +1,37 @@
+package contas
+
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroConta   int
+	saldo         float64
+}
+
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso!"
+	} else {
+		return "Saldo insuficiente."
+	}
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito > 0 {
+		c.saldo += valorDoDeposito
+		return "Depósito realizado com sucesso! Saldo em conta:", c.saldo
+	} else {
+		return "O valor do depósito não pode ser negativo! Saldo em conta:", c.saldo
+	}
+}
+
+func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
+	if valorDaTransferencia < c.saldo && valorDaTransferencia > 0 {
+		c.saldo -= valorDaTransferencia
+		contaDestino.Depositar(valorDaTransferencia)
+		return true
+	} else {
+		return false
+	}
+}
