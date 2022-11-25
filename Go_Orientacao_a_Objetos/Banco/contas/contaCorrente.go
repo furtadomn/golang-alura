4d81cba621ba1dc8d4284af2f979113e744efb69
furtadomn <mnfurtado@yahoo.com.br>
2022-11-25 08:20:30 -0300

    change 'saldo' visibility and create ObterSaldo method

--- Go_Orientacao_a_Objetos/Banco/contas/contaCorrente.go
+++ Go_Orientacao_a_Objetos/Banco/contas/contaCorrente.go
@@ -8,34 +8,38 @@ type ContaCorrente struct {
 	Titular       clientes.Titular
 	NumeroAgencia int
 	NumeroConta   int
-	Saldo         float64
+	saldo         float64
 }
 
 func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.Saldo
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
 	if podeSacar {
-		c.Saldo -= valorDoSaque
+		c.saldo -= valorDoSaque
 		return "Saque realizado com sucesso!"
 	} else {
-		return "Saldo insuficiente."
+		return "saldo insuficiente."
 	}
 }
 
 func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
 	if valorDoDeposito > 0 {
-		c.Saldo += valorDoDeposito
-		return "Depósito realizado com sucesso! Saldo em conta:", c.Saldo
+		c.saldo += valorDoDeposito
+		return "Depósito realizado com sucesso! saldo em conta:", c.saldo
 	} else {
-		return "O valor do depósito não pode ser negativo! Saldo em conta:", c.Saldo
+		return "O valor do depósito não pode ser negativo! saldo em conta:", c.saldo
 	}
 }
 
 func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
-	if valorDaTransferencia < c.Saldo && valorDaTransferencia > 0 {
-		c.Saldo -= valorDaTransferencia
+	if valorDaTransferencia < c.saldo && valorDaTransferencia > 0 {
+		c.saldo -= valorDaTransferencia
 		contaDestino.Depositar(valorDaTransferencia)
 		return true
 	} else {
 		return false
 	}
+}
+
+func (c *ContaCorrente) ObterSaldo() float64 {
+	return c.saldo
 }
--- Go_Orientacao_a_Objetos/Banco/main.go
+++ Go_Orientacao_a_Objetos/Banco/main.go
@@ -3,23 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/furtadomn/golang-studies/Go_Orientacao_a_Objetos/Banco/clientes"
 	"github.com/furtadomn/golang-studies/Go_Orientacao_a_Objetos/Banco/contas"
 )
 
 func main() {
-	contaDaMarcella := contas.ContaCorrente{Titular: clientes.Titular{
-		Nome:      "Marcella",
-		CPF:       "123.123.123-13",
-		Profissao: "Desenvolvedora",
-	},
-		NumeroAgencia: 123,
-		NumeroConta:   123456,
-		Saldo:         1000,
-	}
+	contaExemplo := contas.ContaCorrente{}
+	contaExemplo.Depositar(100)
 
-	// clienteMarcella := clientes.Titular{"Marcella", "123.123.123-13", "Desenvolvedora"}
-	// contaDaMarcella := contas.ContaCorrente{clienteMarcella, 123, 123456, 1000}
-
-	fmt.Println(contaDaMarcella)
+	fmt.Println(contaExemplo.ObterSaldo())
 }
