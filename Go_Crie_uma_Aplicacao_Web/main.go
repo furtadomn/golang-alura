2025c6f9bd16f9c3a3a57bad1e448eae46d8bfe5
furtadomn <mnfurtado@yahoo.com.br>
2022-11-27 18:44:39 -0300

    create 'Produto' struct and fix HTML

--- Go_Crie_uma_Aplicacao_Web/main.go
+++ Go_Crie_uma_Aplicacao_Web/main.go
@@ -5,6 +5,13 @@ import (
 	"net/http"
 )
 
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
 func main() {
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
@@ -13,5 +20,16 @@ func main() {
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+	produtos := []Produto{
+		{
+			Nome:       "Camiseta",
+			Descricao:  "Azul bem bonita",
+			Preco:      49.90,
+			Quantidade: 10,
+		},
+		{"Camiseta", "Confortável", 250.00, 5},
+		{"Fone", "Muito bom", 195.00, 8},
+		{"Garrafa de água", "2 litros", 75.90, 5},
+	}
+	temp.ExecuteTemplate(w, "Index", produtos)
 }
