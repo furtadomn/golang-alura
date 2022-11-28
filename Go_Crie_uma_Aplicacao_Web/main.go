82488e7c718b8fda331b2fce1bbcad70b593c53f
furtadomn <mnfurtado@yahoo.com.br>
2022-11-28 15:34:38 -0300

    create db connection

--- Go_Crie_uma_Aplicacao_Web/main.go
+++ Go_Crie_uma_Aplicacao_Web/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"html/template"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
-type Produto struct {
+type Product struct {
+	Id         int
 	Nome       string
 	Descricao  string
 	Preco      float64
@@ -19,17 +26,56 @@ func main() {
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
-func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{
-			Nome:       "Camiseta",
-			Descricao:  "Azul bem bonita",
-			Preco:      49.90,
-			Quantidade: 10,
-		},
-		{"Camiseta", "Confortável", 250.00, 5},
-		{"Fone", "Muito bom", 195.00, 8},
-		{"Garrafa de água", "2 litros", 75.90, 5},
+func dbConnect() *sql.DB {
+	envErr := godotenv.Load(".env")
+	if envErr != nil {
+		fmt.Println("Could not load .env file")
+		os.Exit(1)
 	}
-	temp.ExecuteTemplate(w, "Index", produtos)
+
+	user := os.Getenv("DBUSER")
+	dbname := os.Getenv("DBNAME")
+	pass := os.Getenv("PASSWORD")
+
+	connect := ("user=" + user + " dbname=" + dbname + " password=" + pass + " host=localhost sslmode=disable")
+
+	db, err := sql.Open("postgres", connect)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	db := dbConnect()
+
+	selectAllProducts, err := db.Query("select * from products")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Product{}
+	products := []Product{}
+
+	for selectAllProducts.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = selectAllProducts.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+
+		products = append(products, p)
+	}
+
+	temp.ExecuteTemplate(w, "Index", products)
+	defer db.Close()
 }
