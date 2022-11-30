6935366d85180b6464c6931f4d069bcaa4601f7a
furtadomn <mnfurtado@yahoo.com.br>
2022-11-30 08:38:03 -0300

    create db folder

---
+++ Go_Crie_uma_Aplicacao_Web/db/db.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func dbConnect() *sql.DB {
+	envErr := godotenv.Load(".env")
+	if envErr != nil {
+		fmt.Println("Could not load .env file")
+		os.Exit(1)
+	}
+
+	user := os.Getenv("DBUSER")
+	dbname := os.Getenv("DBNAME")
+	pass := os.Getenv("PASSWORD")
+
+	connect := ("user=" + user + " dbname=" + dbname + " password=" + pass + " host=localhost sslmode=disable")
+
+	db, err := sql.Open("postgres", connect)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
+}
